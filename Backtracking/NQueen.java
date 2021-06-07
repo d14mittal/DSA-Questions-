2986e2ee45c3160f9bc5e0ed19b34f9fc861e8e4
d14mittal <44315483+d14mittal@users.noreply.github.com>
2021-06-08 01:03:13 +0530

    commit again

--- Backtracking/NQueen.java
+++ Backtracking/NQueen.java
@@ -1,3 +1,3 @@
 public class NQueen{
-    
+    boolean isSafe(int a[][],)
 }
--- Backtracking/RatInMaze.java
+++ Backtracking/RatInMaze.java
@@ -1,23 +1,30 @@
 public class RatInMaze {
     
-    static boolean isSafe(int a[][],int i,int j){   
-        if(i<n && j<n && a[i][j]==1){
+    static boolean isSafe(int a[][],int i,int j, int n){   
+        if(i<n && j<n && i>=0 && j>=0 && a[i][j]==1){
             return true;
         }
         return false;
     }
 
-    maze(int a[][],int i, int j,int solarr[][]){
-        if(isSafe(a, i, j)){
-            solarr[i][j]=1;
+    maze(int a[][],int i, int j,int solvearr[][]){
+
+        if(i==n-1 && j==n-1){
+            solvearr[i][j]=1;
+            return true;
+        }
+        if(isSafe(a, i, j, n)){
+            solvearr[i][j]=1;
             if(maze(a,i+1,j,solvearr)){
                 return true;
             }
+            if(maze(a,i,j+1,solvearr)){
+                return true;
+            }
+            sovlearr[i][j]=0; //backtrack
+            return fasle;
         }
-        if(isSafe(a, row, col+1)){
-            return;
-        }
-
+        return false;
     }
     
     
